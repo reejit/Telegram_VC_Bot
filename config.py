9ce11e49f12ec296fdd1c4b4757a2aa9cae8001d
reejit <70316178+reejit@users.noreply.github.com>
2021-03-10 20:54:47 +0530

    Update and rename sample_config.py to config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-bot_token = "1488727769:AAElRU7yS309GP1t54F635ASDF"
+bot_token = "1370150385:AAH36xDrK0aMeoIrDvfFQ6lAVUKzuVETWPE"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
 sudo_chat_id = -1001485876964
-owner_id = 1243703097
+owner_id = 1229057875
