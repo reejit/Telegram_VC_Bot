d17dc4935119daaa9d8f241858c71c73ba5f6b45
reejit <70316178+reejit@users.noreply.github.com>
2021-03-10 20:59:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 bot_token = "1370150385:AAH36xDrK0aMeoIrDvfFQ6lAVUKzuVETWPE"
 radio_link = "http://peridot.streamguys.com:7150/Mirchi"
-sudo_chat_id = -1001485876964
+sudo_chat_id = -1001248104416
 owner_id = 1229057875
